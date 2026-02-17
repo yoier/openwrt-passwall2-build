102e7bf128b7bcb7d813bcead5c8c8cdd0bc8dfb
Yoier <huaimingdeng@gmail.com>
2026-02-17 23:06:55 +0800

    update url

--- upgrade_passwall2_apk.sh
+++ upgrade_passwall2_apk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir /tmp/upgrade_passwall2_apk
 cd /tmp/upgrade_passwall2_apk
-download_url=$(curl -s https://api.github.com/repos/xiaorouji/openwrt-passwall2/releases/latest | grep "browser_download_url.*passwall2-.*apk"| sed -E 's/.*"([^"]+)".*/\1/')
+download_url=$(curl -s https://api.github.com/repos/Openwrt-Passwall/openwrt-passwall2/releases/latest | grep "browser_download_url.*passwall2-.*apk"| sed -E 's/.*"([^"]+)".*/\1/')
 curl -L -O $(echo $download_url|awk '{print $1}')
 curl -L -O $(echo $download_url|awk '{print $2}')
 apk add /tmp/upgrade_passwall2_apk/*.apk --allow-untrusted
